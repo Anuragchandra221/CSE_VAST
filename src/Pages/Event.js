b37ef1f87cd7af94b0706c46cecc3221cf231e86
Anuragchandra221 <pothaluranurag2002@gmail.com>
2024-02-13 12:48:40 +0530

    Animaze link added

--- src/Pages/Event.js
+++ src/Pages/Event.js
@@ -12,6 +12,8 @@ function Event(props) {
       window.location.replace("https://forms.gle/YCJWpmuouSiWew9s9")
     }else if(props.event=="animaze"){
       window.location.replace("https://forms.gle/g29W5Vji1qP3MW4A8")
+    }else{
+      window.location.replace("https://csevast.in")
     }
   return (
     <div>
