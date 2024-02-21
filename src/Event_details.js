d6f8958f6677b042c968abd367585c247c1c5436
Anuragchandra221 <pothaluranurag2002@gmail.com>
2024-02-21 20:12:05 +0530

    updated

--- src/Event_details.js
+++ src/Event_details.js
@@ -274,7 +274,7 @@ const individual = {
         "contact": [
             "Anurag Chandra - +91 7012597830"
         ],
-        "guidelines": "Event date - Feb 29 & Mar 1.Each team should contain 3 members.Each member has to participate in two sub events.There is a registration fee of Rs 300.All participants are required to bring their college ID card.The winners will be awarded from a prize pool worth 35k.A total of 5 teams with the most cumulative individual points will advance to the next day.Top 3 will be awarded the prize money based on the completion of the treasure hunt.Participation certificates will be provided",
+        "guidelines": "Event date - Feb 29 & Mar 1.Each team should contain 3 members.Each member has to participate in two sub events.There is a registration fee of Rs 300.The winners will be awarded from a prize pool worth 35k.A total of 5 teams with the most cumulative individual points will advance to the next day.Top 3 will be awarded the prize money based on the completion of the treasure hunt.Participation certificates will be provided",
         "sub": [
             {
                 "title": "CODEX",
